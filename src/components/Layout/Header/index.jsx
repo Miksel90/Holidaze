1179136ff9ff27f559e451834e10a2cfb1bb91ce
miksel90 <mikael.selstad@gmail.com>
2024-04-15 21:32:09 +0200

    style

--- src/components/Layout/Header/index.jsx
+++ src/components/Layout/Header/index.jsx
@@ -6,7 +6,7 @@ import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <header className="fixed top-0 left-0 z-30 w-full md:w-[180px] md:h-screen bg-primary shadow-lg ">
-      <div className="bg-primary py-4 px-4 gap-8 md:px-10 flex md:flex-col lg:flex-col justify-between md:items-center lg:items-center lg:justify-center md:space-y-4 lg:space-y-4">
+      <div className="bg-primary py-4 px-4 gap-8 md:px-10 flex md:flex-col justify-between md:items-center lg:justify-center md:space-y-4 ">
         <Link to="/" className="flex md:flex-col justify-center items-center">
           <img
             src={logo}
@@ -18,7 +18,7 @@ const Header = () => {
             Holidaze.
           </span>
         </Link>
-        <div className="flex md:flex-col lg:flex-col justify-end md:justify-center lg:justify-center items-center md:py-8">
+        <div className="flex md:flex-col  justify-end md:justify-center  items-center md:py-8">
           <HeaderNavigation />
         </div>
       </div>
