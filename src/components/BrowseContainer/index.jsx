8f21bd61eff3ba3e7648d60dbd0596988b296cca
miksel90 <mikael.selstad@gmail.com>
2024-04-17 12:28:52 +0200

    added a slideshow to the register page

--- src/components/BrowseContainer/index.jsx
+++ src/components/BrowseContainer/index.jsx
@@ -1,14 +1,31 @@
-import background from "../../assets/Images/hlidaze-3.webp";
+import { useState, useEffect } from "react";
+import background1 from "../../assets/Images/bg1.webp";
+import background2 from "../../assets/Images/bg2.webp";
+import background3 from "../../assets/Images/bg3.webp";
 import { Link } from "react-router-dom";
 import DefaultButton from "../Buttons/DefaultButton";
 
 function BrowseContainer() {
+  const backgrounds = [background1, background2, background3];
+  const [currentBackground, setCurrentBackground] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentBackground(
+        (currentBackground) => (currentBackground + 1) % backgrounds.length
+      );
+    }, 5000);
+
+    return () => clearInterval(interval);
+  }, [currentBackground]);
+
   return (
     <div
       style={{
-        backgroundImage: `url(${background})`,
+        backgroundImage: `url(${backgrounds[currentBackground]})`,
         height: "60vh",
         backgroundSize: "cover",
+        transition: "background-image 3s ease-in-out",
       }}
     >
       <div className="flex flex-col justify-between h-full p-4 gap-8 items-center">
--- src/components/Buttons/DefaultButton/index.jsx
+++ src/components/Buttons/DefaultButton/index.jsx
@@ -4,8 +4,8 @@ import PropTypes from "prop-types";
 const DefaultButton = ({ to, children }) => (
   <Link
     to={to}
-    className="border-2 border-porsche p-2 px-8 rounded text-white text-2xl font-condensed
-  hover:bg-white hover:text-primary hover:border-primary transition-colors duration-300 ease-in-out"
+    className="border-2 bg-white border-porsche p-2 px-8 rounded text-cedar text-2xl font-condensed
+  hover:bg-primary hover:text-white hover:border-primary transition-colors duration-300 ease-in-out"
   >
     {children}
   </Link>
