ea4c5b9b2531694aa6e84a3fa66a6c4039c544c1
miksel90 <mikael.selstad@gmail.com>
2024-05-09 15:59:19 +0200

    style

--- src/components/Navigation/Footer/index.jsx
+++ src/components/Navigation/Footer/index.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const FooterNavigation = () => {
   return (
     <nav className="flex flex-col md:flex-row font-condensed items-center justify-center ">
-      <ul className="flex flex-col md:flex-row text-lg md:text-xl items-center gap-3 md:gap-15 py-2 px-4 bg-white rounded-sm uppercase">
+      <ul className="flex flex-col md:flex-row text-lg md:text-xl items-center gap-3 md:gap-10 py-2 px-4 bg-white rounded-sm uppercase">
         <li className="md:mr-4 hover:underline">
           <Link to="/venues">Browse Venues</Link>
         </li>
