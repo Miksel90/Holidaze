8c7266a5cb500c97f9b0d9441c8b082e636b3135
miksel90 <mikael.selstad@gmail.com>
2024-05-15 11:26:02 +0200

    hamburger menu correclty closes when you click outside it

--- src/components/Navigation/Header/index.jsx
+++ src/components/Navigation/Header/index.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import HamburgerButton from "../../Buttons/Hamburger/index.jsx";
 import { Link } from "react-router-dom";
 import LogoutUser from "../../Auth/Logout/index.jsx";
@@ -6,10 +6,24 @@ import LogoutUser from "../../Auth/Logout/index.jsx";
 const HeaderNavigation = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [hasUserName, setHasUserName] = useState(false);
+  const menuRef = useRef(null);
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
+
+  const handleClickOutside = (event) => {
+    if (menuRef.current && !menuRef.current.contains(event.target)) {
+      setIsMenuOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
 
   useEffect(() => {
     const userName = localStorage.getItem("userName");
@@ -17,7 +31,7 @@ const HeaderNavigation = () => {
   }, []);
 
   return (
-    <nav className="bg-primary font-condensed w-full">
+    <nav className="bg-primary font-condensed w-full" ref={menuRef}>
       <div className="lg:hidden">
         <HamburgerButton isMenuOpen={isMenuOpen} onClick={toggleMenu} />
       </div>
@@ -25,7 +39,7 @@ const HeaderNavigation = () => {
         className={`lg:flex lg:flex-col lg:static lg:w-auto lg:p-0 lg:shadow-none lg:bg-transparent bg-white md:rounded-md
           ${
             isMenuOpen ? "block" : "hidden"
-          } absolute top-full left:0 xxs:-ml-14 md:ml-0 bg-primary transition-all duration-300 ease-in-out shadow-md text-2xl text-cedar p-4`}
+          } absolute top-full left:0 xxs:-ml-14 md:ml-0 bg-primary transition-all duration-500 ease-in-out shadow-md text-2xl text-cedar p-4`}
       >
         <li className="py-2 px-5">
           <Link to="/" className="no-underline hover:underline">
